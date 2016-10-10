d4f7f32b58bc68d59e503abcaf16ecaa6a460714
yahor.subach <yahor.subach@soft-fx.lv>
2016-10-10 18:12:52 +0300

    Added missed files

---
+++ QuoteHistoryGUI/HistoryTools/HistoryParams.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteHistoryGUI.HistoryTools
+{
+    class HistoryParams
+    {
+        
+
+        static public readonly Dictionary<string, byte> periodicityDict = new Dictionary<string, byte>()
+        {
+            {"ticks",0 },
+            {"ticks level2",1 },
+            {"M1 ask",2 },
+            {"M1 bid",3 },
+        };
+
+        static public readonly Dictionary<string, byte> typeDict = new Dictionary<string, byte>()
+        {
+            {"Meta",0 },
+            {"Chunk",1 },
+        };
+    }
+}
---
+++ QuoteHistoryGUI/HistoryTools/HistorySerializer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoteHistoryGUI.HistoryTools
+{
+
+
+    class QHItem
+    {
+        public DateTime Time;
+    }
+    class QHBar: QHItem
+    {
+        public decimal Open;
+        public decimal High;
+        public decimal Low;
+        public decimal Close;
+    }
+    class QHTick : QHItem
+    {
+        public decimal Bid;
+        public decimal BidVolume;
+        public decimal Ask;
+        public decimal AskVolume;
+    }
+
+    class QHTickLevel2 : QHItem
+    {
+        public KeyValuePair<decimal,decimal>[] Bids;
+        public KeyValuePair<decimal, decimal>[] Asks;
+        public KeyValuePair<decimal, decimal> BestBid { get { return Bids.Count()>0?Bids.Last():new KeyValuePair<decimal, decimal>(); } }
+        public KeyValuePair<decimal, decimal> BestAsk { get { return Asks.Count() > 0 ? Asks.Last() : new KeyValuePair<decimal, decimal>();} }
+    }
+
+    class HistorySerializer
+    {
+        public static IEnumerable<QHItem> Deserialize(string period, byte[] content)
+        {
+            if(period == "ticks")
+            {
+                return DeserializeTicks(content);
+            }
+            else if (period == "ticks level2")
+            {
+                return DeserializeTicksLevel2(content);
+            }
+            else if (period == "M1 ask" || period == "M1 bid")
+            {
+                return DeserializeBars(content);
+            }
+
+            return null;
+        }
+
+
+        public static IEnumerable<QHBar> DeserializeBars(byte[] content)
+        {
+            List<QHBar> res = new List<QHBar>();
+            StreamReader reader = new StreamReader(new MemoryStream(content));
+            while (!reader.EndOfStream)
+            {
+                var splittedLine = reader.ReadLine().Split('\t');
+                QHBar bar = new QHBar();
+                bar.Time = DateTime.Parse(splittedLine[0], CultureInfo.InvariantCulture);
+                bar.Open = decimal.Parse(splittedLine[1], CultureInfo.InvariantCulture);
+                bar.High = decimal.Parse(splittedLine[2], CultureInfo.InvariantCulture);
+                bar.Low = decimal.Parse(splittedLine[3], CultureInfo.InvariantCulture);
+                bar.Close = decimal.Parse(splittedLine[4], CultureInfo.InvariantCulture);
+                res.Add(bar);
+            }
+            return res;
+        }
+
+
+        public static IEnumerable<QHTick> DeserializeTicks(byte[] content)
+        {
+            List<QHTick> res = new List<QHTick>();
+            StreamReader reader = new StreamReader(new MemoryStream(content));
+            while (!reader.EndOfStream)
+            {
+                var splittedLine = reader.ReadLine().Split('\t');
+                QHTick tick = new QHTick();
+                tick.Time = DateTime.Parse(splittedLine[0], CultureInfo.InvariantCulture);
+                tick.Bid = decimal.Parse(splittedLine[1], CultureInfo.InvariantCulture);
+                tick.BidVolume = decimal.Parse(splittedLine[2], CultureInfo.InvariantCulture);
+                tick.Ask = decimal.Parse(splittedLine[3], CultureInfo.InvariantCulture);
+                tick.AskVolume = decimal.Parse(splittedLine[4], CultureInfo.InvariantCulture);
+                res.Add(tick);
+            }
+            return res;
+        }
+
+        public static IEnumerable<QHTickLevel2> DeserializeTicksLevel2(byte[] content)
+        {
+            List<QHTickLevel2> res = new List<QHTickLevel2>();
+            StreamReader reader = new StreamReader(new MemoryStream(content));
+            while (!reader.EndOfStream)
+            {
+                var splittedLine = reader.ReadLine().Split('\t');
+                QHTickLevel2 tick = new QHTickLevel2();
+                tick.Time = DateTime.Parse(splittedLine[0], CultureInfo.InvariantCulture);
+                List<KeyValuePair<decimal, decimal>> Bids = new List<KeyValuePair<decimal, decimal>>();
+                List<KeyValuePair<decimal, decimal>> Asks = new List<KeyValuePair<decimal, decimal>>();
+                int i = 1;
+                if(i < splittedLine.Count() && splittedLine[i]=="bid")
+                {
+                    i++;
+                    while(i < splittedLine.Count() && splittedLine[i] != "ask")
+                    {
+                        Bids.Add(new KeyValuePair<decimal, decimal>(decimal.Parse(splittedLine[i], CultureInfo.InvariantCulture), decimal.Parse(splittedLine[i+1], CultureInfo.InvariantCulture)));
+                        i += 2;
+                    }
+                }
+                if (i < splittedLine.Count() && splittedLine[i] == "ask")
+                {
+                    i++;
+                    while (i < splittedLine.Count())
+                    {
+                        Asks.Add(new KeyValuePair<decimal, decimal>(decimal.Parse(splittedLine[i], CultureInfo.InvariantCulture), decimal.Parse(splittedLine[i+1], CultureInfo.InvariantCulture)));
+                        i += 2;
+                    }
+                }
+
+
+                tick.Bids = Bids.ToArray();
+                tick.Asks = Asks.ToArray();
+
+                res.Add(tick);
+            }
+            return res;
+        }
+
+
+    }
+}
