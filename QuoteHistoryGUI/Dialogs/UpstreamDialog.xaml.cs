ac723db81021c211e5e1a37fd2ffdff708786654
yahor.subach <yahor.subach@soft-fx.lv>
2016-12-15 16:08:22 +0300

    add missed files

---
+++ QuoteHistoryGUI/Dialogs/UpstreamDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuoteHistoryGUI.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for UpstreamDialog.xaml
+    /// </summary>
+    public partial class UpstreamDialog : Window
+    {
+        public UpstreamDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void templateHelpButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void templateHelpButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
