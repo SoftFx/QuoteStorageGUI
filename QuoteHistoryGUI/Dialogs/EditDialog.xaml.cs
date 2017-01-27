7bbfb257f6a3565ba98b3e367fbe2390cfbd834d
yahor.subach <yahor.subach@soft-fx.lv>
2017-01-27 15:18:23 +0300

    added missing files

---
+++ QuoteHistoryGUI/Dialogs/EditDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuoteHistoryGUI.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for EditDialog.xaml
+    /// </summary>
+    public partial class EditDialog : Window
+    {
+        public bool CompleteEdit = false;
+        public EditDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CompleteEdit = true;
+            this.Close();
+        }
+    }
+}
